0f4e0b0018f5d3bb25eef1adac1855cbb419cdd1
MeCJay12 <43658705+MeCJay12@users.noreply.github.com>
2024-04-17 15:24:12 -0400

    Inital Commit

---
+++ dockerfile
@@ -0,0 +1,29 @@
+FROM ubuntu:focal
+
+ENV PASSWORD="password"
+ENV WEB_PORT="2463"
+ENV LSA_PORT="9000"
+ENV VERSION="007.018.004.000"
+ENV TERM=xterm
+ENV TZ=UTC
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt -y update && apt -y install wget unzip libldap2-dev
+COPY entrypoint.sh /
+COPY LsiSASH /
+
+RUN mkdir /MSM && \
+	wget -O /MSM.zip https://docs.broadcom.com/docs-and-downloads/raid-controllers/raid-controllers-common-files/"$VERSION"_LSA_Linux-x64.zip && \
+	unzip -d /MSM /MSM.zip && \
+	rm -f /MSM.zip
+WORKDIR /MSM/x64/
+RUN dpkg -i LSA_lib_utils2-8.00-1_amd64.deb && \
+	bash install_deb.sh -s $WEB_PORT $LSA_PORT 2 && \
+	cp /LsiSASH /etc/init.d/LsiSASH && \
+	mkdir -p /usr/local/var/log/ && \
+	touch /usr/local/var/log/slpd.log && \
+	mv /opt/lsi/LSIStorageAuthority /opt/lsi/backup && \
+	rm -rf /MSM
+WORKDIR /
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+nginx_port=$WEB_PORT # Run script will set the LSA webUI port to the value of nginx_port
+LSA_port=$LSA_PORT # Run script will set the LSA control port to the value of LSA_port
+# $ROOT_PASSWORD Will be set as the root password/default login
+# $ADD_USERS_RW List of RW users in space seperated username:password format
+# $ADD_USERS_RO List of RO users in space seperated username:password format
+
+# Function to create webUI passwords
+# $1 = Unparsed, individual user in username:password
+# $2 = Group to add user to; root for RW, users for RO
+add_user () {
+	USR=$(echo "$1" |cut -d ':' -f 1)
+	PASSWD=$(echo "$1" | cut -d ':' -f 2)
+
+	useradd -G "$2" "$USR"
+	echo "$1" | chpasswd
+}
+
+# Copies LSA files in case user mounted empty dir to server dir to svae config files
+if [[ -z $(ls -A /opt/lsi/LSIStorageAuthority) ]]; then
+	mkdir -p /opt/lsi/LSIStorageAuthority/
+	cp -r /opt/lsi/backup/* /opt/lsi/LSIStorageAuthority/
+fi
+
+if [[ ! -z "$ROOT_PASSWORD" ]] ; then
+	echo "root:$ROOT_PASSWORD" | chpasswd
+fi
+
+for USR in ${ADD_USERS_RW[@]} ; do
+	add_user "$USR" "root"
+done
+
+for USR in ${ADD_USERS_RO[@]} ; do
+	add_user "$USR" "users"
+done
+
+/etc/init.d/LsiSASH start
